68fddcc0c514556e4a2d40d0be0e7b251fe75195
Dani Brear <dani@fanscoutapp.com>
2025-11-09 16:58:15 -0500

    fixed some theme stuff

--- app/components/Welcome.tsx
+++ app/components/Welcome.tsx
@@ -11,6 +11,7 @@ import {
   DialogContent,
   DialogTitle,
   Typography,
+  useMediaQuery,
 } from "@mui/material";
 import { theme } from "~/theme";
 import { useLocalStorageState } from "~/utils/useLocalStoredState";
@@ -22,16 +23,22 @@ export default function Welcome() {
     isLoading: isWelcomeLoading,
     isError: isWelcomeError,
   } = useLocalStorageState<boolean>("welcome-shown", true);
+  const isDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
   return (
-    <Dialog open={isWelcomeShown}>
+    <Dialog open={isWelcomeShown && !isWelcomeLoading && !isWelcomeError}>
       <DialogTitle>Welcome to my games!</DialogTitle>
       <DialogContent>
-        <Typography>
+        <Typography sx={{ color: isDarkMode ? "#ddd" : "#333" }}>
           These games are provided for free and are not affiliated with any
           company. Any game state is stored in your browser and nothing is ever
           tracked. Enjoy!
         </Typography>
-        <Typography sx={{ textAlign: "right" }}>
+        <Typography
+          sx={{
+            textAlign: "right",
+            color: isDarkMode ? "#ddd" : "#333",
+            mt: 1,
+          }}>
           <FontAwesomeIcon
             icon={faHeart}
             bounce
@@ -47,12 +54,21 @@ export default function Welcome() {
         <Button
           onClick={() => setIsWelcomeShown(false)}
           variant="contained"
-          sx={{ fontWeight: "bold" }}
+          sx={{
+            fontWeight: "bold",
+            textShadow: "1px 1px 2px rgba(0,0,0,.5)",
+          }}
           startIcon={<FontAwesomeIcon icon={faGlasses} />}
           fullWidth>
           Okay nerd, lemme play!
         </Button>
-        <Typography variant="caption" sx={{ opacity: 0.666 }}>
+        <Typography
+          variant="caption"
+          sx={{
+            opacity: 0.666,
+            textAlign: "center",
+            color: isDarkMode ? "#ddd" : "#333",
+          }}>
           (I love being a nerd! ~DB)
           <FontAwesomeIcon
             style={{ marginLeft: 4, marginTop: 10 }}
